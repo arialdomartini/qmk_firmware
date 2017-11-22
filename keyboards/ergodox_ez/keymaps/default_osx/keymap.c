7285bf05ed2f5350be7e2f0449d3ed5929594a95
Arialdo Martini <arialdomartini@gmail.com>
2017-11-22 11:16:58 +0100

    E and I are mapped as Cmd (GUI)

--- keyboards/ergodox_ez/keymaps/default_osx/keymap.c
+++ keyboards/ergodox_ez/keymaps/default_osx/keymap.c
@@ -35,17 +35,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_EQL,          KC_1,         KC_2,               KC_3,    KC_4,        KC_5,           KC_LEFT,
-        KC_DELT,         KC_Q,         KC_W,               KC_E,    KC_R,        KC_T,           TG(SYMB),
+        KC_EQL,          KC_1,         KC_2,               KC_3,                 KC_4,        KC_5,           KC_LEFT,
+        KC_DELT,         KC_Q,         KC_W,               MT(MOD_LGUI, KC_E),   KC_R,        KC_T,           TG(SYMB),
         KC_BSPC,         KC_A,         MT(MOD_LALT,KC_S),  MT(MOD_LCTL, KC_D),   MT(MOD_LSFT, KC_F),   KC_G,
-        KC_LSFT,         KC_Z,         KC_X,               KC_C,    KC_V,        KC_B,           ALL_T(KC_NO),
-        LT(SYMB,KC_GRV), KC_QUOT,      LALT(KC_LSFT),      KC_LEFT, KC_RGHT,
-                                                                                 ALT_T(KC_APP),  KC_LGUI,
-                                                                                                 KC_HOME,
-                                                                    KC_SPC,      KC_BSPC,        KC_END,
+        KC_LSFT,         KC_Z,         KC_X,               KC_C,                 KC_V,        KC_B,           ALL_T(KC_NO),
+        LT(SYMB,KC_GRV), KC_QUOT,      LALT(KC_LSFT),      KC_LEFT,              KC_RGHT,
+                                                                                              ALT_T(KC_APP),  KC_LGUI,
+                                                                                                              KC_HOME,
+                                                                                 KC_SPC,      KC_BSPC,        KC_END,
         // right hand
         KC_RGHT,         KC_6,          KC_7,                KC_8,                 KC_9,                   KC_0,               KC_MINS,
-        TG(SYMB),        KC_Y,          KC_U,                KC_I,                 KC_O,                   KC_P,               KC_BSLS,
+        TG(SYMB),        KC_Y,          KC_U,                MT(MOD_RGUI, KC_I),   KC_O,                   KC_P,               KC_BSLS,
                          KC_H,          MT(MOD_RSFT, KC_J),  MT(MOD_RCTL, KC_K),   MT(MOD_RALT, KC_L),     LT(MDIA, KC_SCLN),  KC_LGUI,
         MEH_T(KC_NO),    KC_N,          KC_M,                KC_COMM,              KC_DOT,                 KC_SLSH,            KC_RSFT,
                                         KC_UP,               KC_DOWN,              KC_LBRC,                KC_RBRC,            KC_FN1,
