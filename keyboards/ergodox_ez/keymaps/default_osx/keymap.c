f27d99cd29559901a6371a44f03187da07d179ad
Arialdo Martini <arialdomartini@gmail.com>
2017-11-22 09:30:41 +0100

    Z and / are not mapped as Ctrl anymore

--- keyboards/ergodox_ez/keymaps/default_osx/keymap.c
+++ keyboards/ergodox_ez/keymaps/default_osx/keymap.c
@@ -15,11 +15,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-------------------------------------------------------.           ,-------------------------------------------------------.
  * |   =    |   1  |    2   |    3   |   4   |   5  | LEFT |           | RIGHT|   6  |   7   |    8   |    9   |   0  |   -    |
  * |--------+------+--------+--------+-------+-------------|           |------+------+-------+--------+--------+------+--------|
- * | Del    |   Q  |    W   |    E   |   R   |   T  |  L1  |           |  L1  |   Y  |   U   |    I   |    O   |   P  |   \    |
+ * | Del    |   Q  |    W   | E/LGui |   R   |   T  |  L1  |           |  L1  |   Y  |   U   | I/RGui |    O   |   P  |   \    |
  * |--------+------+--------+--------+-------+------|      |           |      |------+-------+--------+--------+------+--------|
  * | BkSp   |   A  | S/LAlt | D/LCtl | F/LSh |   G  |------|           |------|   H  | J/RSh | K/RCtl | L/RAlt |; / L2|  LGui  |
  * |--------+------+--------+--------+-------+------| Hyper|           | Meh  |------+-------+--------+--------+------+--------|
- * | LShift |Z/Ctrl|    X   |    C   |   V   |   B  |      |           |      |   N  |   M   |    ,   |    .   |//Ctrl| RShift |
+ * | LShift |   Z  |    X   |    C   |   V   |   B  |      |           |      |   N  |   M   |    ,   |    .   |  /   | RShift |
  * `--------+------+--------+--------+-------+-------------'           `-------------+-------+--------+--------+------+--------'
  *   |Grv/L1|  '"  | AltShf |  Left  | Right |                                       |  Up   |  Down  |    [   |   ]  | ~L1  |
  *   `---------------------------------------'                                       `---------------------------------------'
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_EQL,          KC_1,         KC_2,               KC_3,    KC_4,        KC_5,           KC_LEFT,
         KC_DELT,         KC_Q,         KC_W,               KC_E,    KC_R,        KC_T,           TG(SYMB),
         KC_BSPC,         KC_A,         MT(MOD_LALT,KC_S),  MT(MOD_LCTL, KC_D),   MT(MOD_LSFT, KC_F),   KC_G,
-        KC_LSFT,         CTL_T(KC_Z),  KC_X,               KC_C,    KC_V,        KC_B,           ALL_T(KC_NO),
+        KC_LSFT,         KC_Z,         KC_X,               KC_C,    KC_V,        KC_B,           ALL_T(KC_NO),
         LT(SYMB,KC_GRV), KC_QUOT,      LALT(KC_LSFT),      KC_LEFT, KC_RGHT,
                                                                                  ALT_T(KC_APP),  KC_LGUI,
                                                                                                  KC_HOME,
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_RGHT,         KC_6,          KC_7,                KC_8,                 KC_9,                   KC_0,               KC_MINS,
         TG(SYMB),        KC_Y,          KC_U,                KC_I,                 KC_O,                   KC_P,               KC_BSLS,
                          KC_H,          MT(MOD_RSFT, KC_J),  MT(MOD_RCTL, KC_K),   MT(MOD_RALT, KC_L),     LT(MDIA, KC_SCLN),  KC_LGUI,
-        MEH_T(KC_NO),    KC_N,          KC_M,                KC_COMM,              KC_DOT,                 CTL_T(KC_SLSH),     KC_RSFT,
+        MEH_T(KC_NO),    KC_N,          KC_M,                KC_COMM,              KC_DOT,                 KC_SLSH,            KC_RSFT,
                                         KC_UP,               KC_DOWN,              KC_LBRC,                KC_RBRC,            KC_FN1,
         KC_LALT,         CTL_T(KC_ESC),
         KC_PGUP,
