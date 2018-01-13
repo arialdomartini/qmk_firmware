a0552f3519cf1aef22e130bc1ddeb8dab21b9c04
Arialdo Martini <arialdomartini@gmail.com>
2018-01-13 20:55:08 +0100

    shift = f/j, ctrl = d/k, alt = s/l

--- keyboards/tada68/config.h
+++ keyboards/tada68/config.h
@@ -81,4 +81,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define NO_ACTION_MACRO
 //#define NO_ACTION_FUNCTION
 
+#define IGNORE_MOD_TAP_INTERRUPT
+
 #endif
--- keyboards/tada68/keymaps/default/keymap.c
+++ keyboards/tada68/keymaps/default/keymap.c
@@ -17,6 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |Del |
    * |----------------------------------------------------------------|
    * |CAPS   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |PgUp|
+   * |       |   |ALT|CTL|SFT|   |   |SFT|CTL|ALT|   |   |       |    |
    * |----------------------------------------------------------------|
    * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift | Up|PgDn|
    * |----------------------------------------------------------------|
@@ -24,11 +25,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `----------------------------------------------------------------'
    */
 [_BL] = KEYMAP_ANSI(
-  KC_ESC,    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC,KC_GRV, \
-  KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS,KC_DEL, \
-  KC_CAPS, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,         KC_ENT,KC_PGUP,  \
-  KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,   KC_RSFT,KC_UP,KC_PGDN, \
-  KC_LCTL, KC_LGUI,KC_LALT,                KC_SPC,                        KC_RALT,MO(_FL),KC_RCTRL, KC_LEFT,KC_DOWN,KC_RGHT),
+  KC_ESC,  KC_1,   KC_2,                KC_3,                 KC_4,                KC_5,   KC_6,   KC_7,               KC_8,                 KC_9,               KC_0,      KC_MINS, KC_EQL,     KC_BSPC, KC_GRV, \
+  KC_TAB,  KC_Q,   KC_W,                KC_E,                 KC_R,                KC_T,   KC_Y,   KC_U,               KC_I,                 KC_O,               KC_P,      KC_LBRC, KC_RBRC,    KC_BSLS, KC_DEL, \
+  KC_CAPS, KC_A,   MT(MOD_LALT,KC_S),   MT(MOD_LCTL, KC_D),   MT(MOD_LSFT, KC_F),  KC_G,   KC_H,   MT(MOD_RSFT, KC_J), MT(MOD_RCTL,KC_K),    MT(MOD_RALT,KC_L),  KC_SCLN,   KC_QUOT,             KC_ENT,  KC_PGUP, \
+  KC_LSFT, KC_Z,   KC_X,                KC_C,                 KC_V,                KC_B,   KC_N,   KC_M,               KC_COMM,              KC_DOT,             KC_SLSH,   KC_RSFT,             KC_UP,   KC_PGDN, \
+  KC_LCTL, KC_LGUI,KC_LALT,                        KC_SPC,                                                             KC_RALT,              MO(_FL),            KC_RCTRL,  KC_LEFT,             KC_DOWN,  KC_RGHT),
 
   /* Keymap _FL: Function Layer
    * ,----------------------------------------------------------------.
