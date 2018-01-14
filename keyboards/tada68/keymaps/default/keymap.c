f9f46031c92f93c384e790db224d37cf5b1f8ea4
Arialdo Martini <arialdomartini@gmail.com>
2018-01-14 09:19:57 +0100

    Ordinary Return is disabled

--- keyboards/tada68/keymaps/default/keymap.c
+++ keyboards/tada68/keymaps/default/keymap.c
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |----------------------------------------------------------------|
    * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |Del |
    * |----------------------------------------------------------------|
-   * |       |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |PgUp|
+   * |       |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|       |PgUp|
    * |       |_FL|ALT|CTL|SFT|   |   |SFT|CTL|ALT|   |   |       |    |
    * |----------------------------------------------------------------|
    * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift | Up|PgDn|
@@ -25,10 +25,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `----------------------------------------------------------------'
    */
 [_BL] = KEYMAP_ANSI(
-  KC_ESC,  KC_1,         KC_2,                KC_3,                 KC_4,                KC_5,   KC_6,   KC_7,               KC_8,                 KC_9,               KC_0,      KC_MINS, KC_EQL,     KC_BSPC, KC_GRV, \
-  KC_TAB,  KC_Q,         KC_W,                KC_E,                 KC_R,                KC_T,   KC_Y,   KC_U,               KC_I,                 KC_O,               KC_P,      KC_LBRC, KC_RBRC,    KC_BSLS, KC_DEL, \
-  KC_NO  , LT(_FL,KC_A), MT(MOD_LALT,KC_S),   MT(MOD_LCTL, KC_D),   MT(MOD_LSFT, KC_F),  KC_G,   KC_H,   MT(MOD_RSFT, KC_J), MT(MOD_RCTL,KC_K),    MT(MOD_RALT,KC_L),  KC_SCLN,   KC_QUOT,             KC_ENT,  KC_PGUP, \
-  KC_LSFT, KC_Z,         KC_X,                KC_C,                 KC_V,                KC_B,   KC_N,   KC_M,               KC_COMM,              KC_DOT,             KC_SLSH,   KC_RSFT,             KC_UP,   KC_PGDN, \
+  KC_ESC,  KC_1,         KC_2,                KC_3,                 KC_4,                KC_5,   KC_6,   KC_7,               KC_8,                 KC_9,               KC_0,      KC_MINS, KC_EQL,     KC_BSPC,  KC_GRV, \
+  KC_TAB,  KC_Q,         KC_W,                KC_E,                 KC_R,                KC_T,   KC_Y,   KC_U,               KC_I,                 KC_O,               KC_P,      KC_LBRC, KC_RBRC,    KC_BSLS,  KC_DEL, \
+  KC_NO  , LT(_FL,KC_A), MT(MOD_LALT,KC_S),   MT(MOD_LCTL, KC_D),   MT(MOD_LSFT, KC_F),  KC_G,   KC_H,   MT(MOD_RSFT, KC_J), MT(MOD_RCTL,KC_K),    MT(MOD_RALT,KC_L),  KC_SCLN,   KC_QUOT,             KC_NO  ,  KC_PGUP, \
+  KC_LSFT, KC_Z,         KC_X,                KC_C,                 KC_V,                KC_B,   KC_N,   KC_M,               KC_COMM,              KC_DOT,             KC_SLSH,   KC_RSFT,             KC_UP  ,  KC_PGDN, \
   KC_LCTL, KC_LGUI,      KC_LALT,                        KC_SPC,                                                             KC_RALT,              MO(_FL),            KC_RCTRL,  KC_LEFT,             KC_DOWN,  KC_RGHT),
 
   /* Keymap _FL: Function Layer
