23fb3fce15d8d1c6a9a55c67a32cc423fcdbc004
Arialdo Martini <arialdomartini@gmail.com>
2018-01-14 09:22:24 +0100

    Ordinary Backspace and Del are disabled

--- keyboards/tada68/keymaps/default/keymap.c
+++ keyboards/tada68/keymaps/default/keymap.c
@@ -12,9 +12,9 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _BL: (Base Layer) Default Layer
    * ,----------------------------------------------------------------.
-   * |Esc | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |~ ` |
+   * |Esc | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|       |~ ` |
    * |----------------------------------------------------------------|
-   * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |Del |
+   * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |    |
    * |----------------------------------------------------------------|
    * |       |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|       |PgUp|
    * |       |_FL|ALT|CTL|SFT|   |   |SFT|CTL|ALT|   |   |       |    |
@@ -25,8 +25,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `----------------------------------------------------------------'
    */
 [_BL] = KEYMAP_ANSI(
-  KC_ESC,  KC_1,         KC_2,                KC_3,                 KC_4,                KC_5,   KC_6,   KC_7,               KC_8,                 KC_9,               KC_0,      KC_MINS, KC_EQL,     KC_BSPC,  KC_GRV, \
-  KC_TAB,  KC_Q,         KC_W,                KC_E,                 KC_R,                KC_T,   KC_Y,   KC_U,               KC_I,                 KC_O,               KC_P,      KC_LBRC, KC_RBRC,    KC_BSLS,  KC_DEL, \
+  KC_ESC,  KC_1,         KC_2,                KC_3,                 KC_4,                KC_5,   KC_6,   KC_7,               KC_8,                 KC_9,               KC_0,      KC_MINS, KC_EQL,     KC_NO  ,  KC_GRV, \
+  KC_TAB,  KC_Q,         KC_W,                KC_E,                 KC_R,                KC_T,   KC_Y,   KC_U,               KC_I,                 KC_O,               KC_P,      KC_LBRC, KC_RBRC,    KC_BSLS,  KC_NO , \
   KC_NO  , LT(_FL,KC_A), MT(MOD_LALT,KC_S),   MT(MOD_LCTL, KC_D),   MT(MOD_LSFT, KC_F),  KC_G,   KC_H,   MT(MOD_RSFT, KC_J), MT(MOD_RCTL,KC_K),    MT(MOD_RALT,KC_L),  KC_SCLN,   KC_QUOT,             KC_NO  ,  KC_PGUP, \
   KC_NO  , KC_Z,         KC_X,                KC_C,                 KC_V,                KC_B,   KC_N,   KC_M,               KC_COMM,              KC_DOT,             KC_SLSH,   KC_NO  ,             KC_UP  ,  KC_PGDN, \
   KC_NO  , KC_LGUI,      KC_NO  ,                        KC_SPC,                                                             KC_NO,                MO(_FL),            KC_NO   ,  KC_LEFT,             KC_DOWN,  KC_RGHT),
