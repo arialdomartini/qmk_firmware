1f68de36cdd809126ab3d34df5989e98dd88bee4
Arialdo Martini <arialdomartini@gmail.com>
2018-01-15 18:11:25 +0100

    Return and Backspace in the ordinary position

--- keyboards/tada68/keymaps/default/keymap.c
+++ keyboards/tada68/keymaps/default/keymap.c
@@ -13,12 +13,12 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _BL: (Base Layer) Default Layer
    * ,-----------------------------------------------------------------.
-   * |Esc | 1|  2|  3 |  4|  5|  6|  7|  8 |  9|  0|  -|  =|       |~ ` |
+   * |Esc | 1|  2|  3 |  4|  5|  6|  7|  8 |  9|  0|  -|  =| Bspc  |~ ` |
    * |------------------------------------------------------------------|
    * |Tab  |  Q|  W|   E|  R|  T|  Y|  U|  I |  O|  P|  [|  ]|  \  |    |
    * |     |_CL|   |LGUI|   |   |   |   |RGUI|   |_CL|   |   |     |    |
    * |------------------------------------------------------------------|
-   * | CAPS  |  A|  S|   D|  F|  G|  H|  J|   K|  L|  ;|  '|       |    |
+   * | CAPS  |  A|  S|   D|  F|  G|  H|  J|   K|  L|  ;|  '|Enter  |    |
    * |       |_FL|ALT|CTL |SFT|   |   |SFT|CTL |ALT|_FL|   |       |    |
    * |------------------------------------------------------------------|
    * |        |  Z|  X|   C|  V|  B|  N|  M|  , |  .|  /|      | Up|    |
@@ -27,9 +27,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------------------------'
    */
 [_BL] = KEYMAP_ANSI(
-  KC_ESC,  KC_1,         KC_2,                KC_3,                 KC_4,                KC_5,   KC_6,   KC_7,               KC_8,                 KC_9,               KC_0,            KC_MINS, KC_EQL,     KC_NO  ,  KC_GRV, \
+  KC_ESC,  KC_1,         KC_2,                KC_3,                 KC_4,                KC_5,   KC_6,   KC_7,               KC_8,                 KC_9,               KC_0,            KC_MINS, KC_EQL,     KC_BSPC,  KC_GRV, \
   KC_TAB,  LT(_CL,KC_Q), KC_W,                MT(MOD_LGUI,KC_E),    KC_R,                KC_T,   KC_Y,   KC_U,               MT(MOD_RGUI,KC_I),    KC_O,               LT(_CL,KC_P),    KC_LBRC, KC_RBRC,    KC_BSLS,  KC_NO , \
-  KC_CAPS, LT(_FL,KC_A), MT(MOD_LALT,KC_S),   MT(MOD_LCTL, KC_D),   MT(MOD_LSFT, KC_F),  KC_G,   KC_H,   MT(MOD_RSFT, KC_J), MT(MOD_RCTL,KC_K),    MT(MOD_RALT,KC_L),  LT(_FL,KC_SCLN), KC_QUOT,             KC_NO  ,  KC_NO, \
+  KC_CAPS, LT(_FL,KC_A), MT(MOD_LALT,KC_S),   MT(MOD_LCTL, KC_D),   MT(MOD_LSFT, KC_F),  KC_G,   KC_H,   MT(MOD_RSFT, KC_J), MT(MOD_RCTL,KC_K),    MT(MOD_RALT,KC_L),  LT(_FL,KC_SCLN), KC_QUOT,             KC_ENT ,  KC_NO, \
   KC_NO  , KC_Z,         KC_X,                KC_C,                 KC_V,                KC_B,   KC_N,   KC_M,               KC_COMM,              KC_DOT,             KC_SLSH,         KC_NO  ,             KC_UP  ,  KC_NO, \
   KC_NO  , KC_LGUI,      KC_NO  ,                        KC_SPC,                                                             KC_NO,                MO(_FL),            KC_NO   ,        KC_LEFT,             KC_DOWN,  KC_RGHT),
 
