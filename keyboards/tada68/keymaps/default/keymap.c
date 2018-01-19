787934dbfd181074b938e285bc0ebfb2a4621bcf
Canicatti Alessandro <alessandro.canicatti@tcpos.com>
2018-01-19 08:01:19 +0100

    Right Alt/App

--- keyboards/tada68/keymaps/default/keymap.c
+++ keyboards/tada68/keymaps/default/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------------------------------------------------------------------|
    * |        |  Z|  X|   C|  V|  B|  N|  M|  , |  .|  /|      | Up|    |
    * |------------------------------------------------------------------|
-   * |    |Win |    |         Space          |    | FN|    |Lef|Dow|Rig |
+   * |    |Win |    |         Space          |App | FN|    |Lef|Dow|Rig |
    * `------------------------------------------------------------------'
    */
 [_BL] = KEYMAP_ANSI(
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  LT(_CL,KC_Q), KC_W,                MT(MOD_LGUI,KC_E),    KC_R,                KC_T,   KC_Y,   KC_U,               MT(MOD_RGUI,KC_I),    KC_O,               LT(_CL,KC_P),    KC_LBRC, KC_RBRC,    KC_BSLS,  KC_NO , \
   KC_CAPS, LT(_FL,KC_A), MT(MOD_LALT,KC_S),   MT(MOD_LCTL, KC_D),   MT(MOD_LSFT, KC_F),  KC_G,   KC_H,   MT(MOD_RSFT, KC_J), MT(MOD_RCTL,KC_K),    MT(MOD_LALT,KC_L),  LT(_FL,KC_SCLN), KC_QUOT,             KC_ENT ,  KC_NO, \
   KC_NO  , KC_Z,         KC_X,                KC_C,                 KC_V,                KC_B,   KC_N,   KC_M,               KC_COMM,              KC_DOT,             KC_SLSH,         KC_NO  ,             KC_UP  ,  KC_NO, \
-  KC_NO  , KC_LGUI,      KC_NO  ,                        KC_SPC,                                                             KC_NO,                MO(_FL),            KC_NO   ,        KC_LEFT,             KC_DOWN,  KC_RGHT),
+  KC_NO  , KC_LGUI,      KC_NO  ,                        KC_SPC,                                                             KC_APP,               MO(_FL),            KC_NO   ,        KC_LEFT,             KC_DOWN,  KC_RGHT),
 
   /* Keymap _FL: Function Layer
    * ,------------------------------------------------------------------.
