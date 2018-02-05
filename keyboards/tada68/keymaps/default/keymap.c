a00a2cea88cddac2eb120cf43ed7728e4f10ed56
Arialdo Martini <arialdomartini@gmail.com>
2018-02-05 11:13:12 +0100

    Ins under second key of the left column

--- keyboards/tada68/keymaps/default/keymap.c
+++ keyboards/tada68/keymaps/default/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,-----------------------------------------------------------------.
    * |Esc | 1|  2|  3 |  4|  5|  6|  7|  8 |  9|  0|  -|  =| Bspc  |~ ` |
    * |------------------------------------------------------------------|
-   * |Tab  |  Q|  W|   E|  R|  T|  Y|  U|  I |  O|  P|  [|  ]|  \  |    |
+   * |Tab  |  Q|  W|   E|  R|  T|  Y|  U|  I |  O|  P|  [|  ]|  \  |Ins |
    * |     |   |   |LGUI|   |   |   |   |RGUI|   |   |   |   |     |    |
    * |------------------------------------------------------------------|
    * | CAPS  |  A|  S|   D|  F|  G|  H|  J|   K|  L|  ;|  '|Enter  |    |
@@ -28,14 +28,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 [_BL] = KEYMAP_ANSI(
   KC_ESC  ,  KC_1,         KC_2,                KC_3,                 KC_4,                KC_5,   KC_6,   KC_7,               KC_8,                 KC_9,               KC_0,            KC_MINS, KC_EQL,     KC_BSPC,  KC_GRV, \
-  KC_TAB  ,         KC_Q , KC_W,                MT(MOD_LGUI,KC_E),    KC_R,                KC_T,   KC_Y,   KC_U,               MT(MOD_RGUI,KC_I),    KC_O,               KC_P        ,    KC_LBRC, KC_RBRC,    KC_BSLS,  KC_NO , \
+  KC_TAB  ,         KC_Q , KC_W,                MT(MOD_LGUI,KC_E),    KC_R,                KC_T,   KC_Y,   KC_U,               MT(MOD_RGUI,KC_I),    KC_O,               KC_P        ,    KC_LBRC, KC_RBRC,    KC_BSLS,  KC_INS , \
   KC_CAPS , LT(_ML,KC_A), MT(MOD_LALT,KC_S),   MT(MOD_LCTL, KC_D),   MT(MOD_LSFT, KC_F),  KC_G,   KC_H,   MT(MOD_RSFT, KC_J), MT(MOD_RCTL,KC_K),    MT(MOD_LALT,KC_L),  LT(_ML,KC_SCLN), KC_QUOT,             KC_ENT ,  KC_NO, \
   KC_NO   , KC_Z,         KC_X,                KC_C,                 KC_V,                KC_B,   KC_N,   KC_M,               KC_COMM,              KC_DOT,             KC_SLSH,         KC_NO  ,             KC_UP  ,  KC_NO, \
   KC_NO,   KC_LGUI,      KC_NO  ,                        LT(_FL,KC_SPC),                                                      KC_APP,               KC_NO,              KC_NO   ,        KC_LEFT,             KC_DOWN,  KC_RGHT),
 
   /* Keymap _ML: Movement Layer
    * ,-------------------------------------0------------------------------.
-   * |   | F1|F2 |F3 |F4 |F5 |F6 |F7 |F8  |F9   |F10|F11|F12|        |Ins |
+   * |   | F1|F2 |F3 |F4 |F5 |F6 |F7 |F8  |F9   |F10|F11|F12|        |    |
    * |--------------------------------------------------------------------|
    * |     |   |   |   |   |   |    | PgUp|Up |PdDn|   |   |   |     |    |
    * |--------------------------------------------------------------------|
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `--------------------------------------------------------------------'
    */
 [_ML] = KEYMAP_ANSI(
-  _______,  KC_F1 , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9   , KC_F10 , KC_F11,  KC_F12 , _______,   KC_INS ,  \
+  _______,  KC_F1 , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9   , KC_F10 , KC_F11,  KC_F12 , _______, _______ ,  \
   _______, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_UP  , KC_PGDN , _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, KC_END , _______, _______, _______, \
   _______, _______, _______, BL_DEC , BL_TOGG, BL_INC , _______, KC_BSPC, _______, KC_DEL  , _______, KC_BTN1, KC_MS_U, KC_BTN2, \
