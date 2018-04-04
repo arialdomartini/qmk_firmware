86ea5c43599576d0bf0b141f691b5d674aa9071d
Alessandro Canicatti <ales.canicatti@gmail.com>
2018-04-04 21:23:17 +0200

    Backspace instead of caps lock

--- keyboards/whitefox/keymaps/default/keymap.c
+++ keyboards/whitefox/keymaps/default/keymap.c
@@ -21,9 +21,9 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------------------------------------------------------------------------------------------|
      * |Tab  |    Q|    W|    E|    R|    T|    Y|    U|    I|    O|    P|    [|    ]|Backs|   Del|
      * |------------------------------------------------------------------------------------------|
-     * |CapsLo|A/CRS|S/ALT|D/CTL|F/SFT|    G|    H|J/SFT|D/CTL|L/ALT|    ;|    '|  Enter   |   PgU|
+     * |BackSp|A/CRS|S/ALT|D/CTL|F/SFT|    G|    H|J/SFT|D/CTL|L/ALT|    ;|    '|  Enter   |   PgU|
      * |------------------------------------------------------------------------------------------|
-     * |Shif|     |    Z|    X|    C|    V|    B|    N|    M|    ,|    .|    /| Shift | Up |   PgD|
+     * |Shif|     |    Z|    X|    C|    V|    B|    N|    M|    ,|    .|    /| Caps  | Up |   PgD|
      * |------------------------------------------------------------------------------------------|
      * |Ctrl| Gui  |Alt  |           Space                | Fn0 | Alt | Gui |    | Lef | Dow | Rig|
      * `------------------------------------------------------------------------------------------'
@@ -31,8 +31,8 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = KEYMAP(
         KC_ESC,  KC_1   , KC_2   , KC_3 , KC_4 , KC_5 , KC_6 , KC_7 , KC_8 , KC_9   , KC_0   , KC_MINS, KC_EQL,  KC_BSLS, KC_GRV , KC_INS,
         KC_TAB,  KC_Q   , KC_W   , KC_E , KC_R , KC_T , KC_Y , KC_U , KC_I , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSPC         , KC_DEL,
-        KC_CAPS, MY_A   , KC_S   , MY_D , MY_F , KC_G , KC_H , MY_J , MY_K , KC_L   , KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT          , KC_PGUP,
-        KC_LSFT, KC_NUBS, KC_Z   , KC_X , KC_C , KC_V , KC_B , KC_N , KC_M , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT         , KC_UP  , KC_PGDN,
+        KC_BSPC, MY_A   , MY_S   , MY_D , MY_F , KC_G , KC_H , MY_J , MY_K , MY_L   , KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT          , KC_PGUP,
+        KC_LSFT, KC_NUBS, KC_Z   , KC_X , KC_C , KC_V , KC_B , KC_N , KC_M , KC_COMM, KC_DOT , KC_SLSH, KC_CAPS         , KC_UP  , KC_PGDN,
         KC_LCTL, KC_LGUI, KC_LALT,                  KC_SPC                          , KC_RALT, KC_FN0 , _______, KC_LEFT, KC_DOWN, KC_RGHT),
 
 
