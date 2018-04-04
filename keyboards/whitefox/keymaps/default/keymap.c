9a5c2cd525a2e2f0f9551f1432f05f3bc3027061
Alessandro Canicatti <ales.canicatti@gmail.com>
2018-04-04 21:17:22 +0200

    Added Cursor layout

--- keyboards/whitefox/keymaps/default/keymap.c
+++ keyboards/whitefox/keymaps/default/keymap.c
@@ -4,7 +4,9 @@
 #define CURS 1 // symbols
 #define MDIA 2 // media keys
 
-#DEFINE _______ KC_TRNS
+#define _______ KC_TRNS
+
+#define MY_A LT(CURS, KC_A)
 #define MY_F MT(MOD_LSFT, KC_F)
 #define MY_J MT(MOD_LSFT, KC_J)
 #define MY_D MT(MOD_LCTL, KC_D)
@@ -27,20 +29,20 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = KEYMAP(
         KC_ESC,  KC_1   , KC_2   , KC_3 , KC_4 , KC_5 , KC_6 , KC_7 , KC_8 , KC_9   , KC_0   , KC_MINS, KC_EQL,  KC_BSLS, KC_GRV , KC_INS,
         KC_TAB,  KC_Q   , KC_W   , KC_E , KC_R , KC_T , KC_Y , KC_U , KC_I , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSPC         , KC_DEL,
-        KC_CAPS, KC_A   , KC_S   , MY_D , MY_F , KC_G , KC_H , MY_J , MY_K , KC_L   , KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT          , KC_PGUP,
-        KC_LSFT, KC_NUBS, KC_Z   , KC_X , KC_C , KC_V , KC_B , KC_N , KC_M , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, KC_UP  , KC_PGDN,
+        KC_CAPS, MY_A   , KC_S   , MY_D , MY_F , KC_G , KC_H , MY_J , MY_K , KC_L   , KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT          , KC_PGUP,
+        KC_LSFT, KC_NUBS, KC_Z   , KC_X , KC_C , KC_V , KC_B , KC_N , KC_M , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT         , KC_UP  , KC_PGDN,
         KC_LCTL, KC_LGUI, KC_LALT,                  KC_SPC                          , KC_RALT, KC_FN0 , _______, KC_LEFT, KC_DOWN, KC_RGHT),
 
 
 
     [CURS] = KEYMAP(
-        _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______, _______, KC_MUTE,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLU,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_VOLD,
-        _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_END),
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, KC_HOME,  KC_UP , KC_END , _______, _______, _______, _______         , _______,
+        _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______         , _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP, _______, KC_PGDN, _______, _______         , _______, _______,
+        _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______, _______)
 };
 
 const uint16_t fn_actions[] = {
-    [0] = ACTION_LAYER_MOMENTARY(CURSXS),
+    [0] = ACTION_LAYER_MOMENTARY(CURS),
 };
