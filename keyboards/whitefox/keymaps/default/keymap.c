db87726feb718f40bae2f94dd2cd29d3fbb5e4af
Arialdo Martini <arialdomartini@gmail.com>
2018-04-05 07:56:06 +0200

    backspace and del on left and right shifts

--- keyboards/whitefox/keymaps/default/keymap.c
+++ keyboards/whitefox/keymaps/default/keymap.c
@@ -27,13 +27,13 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,--------------------------------------------------------------------.
    * |Esc|  1|  2|    3|  4|  5|  6|  7|    8|  9|  0|  -|  =|  \|  `|    |
    * |--------------------------------------------------------------------|
-   * |Tab  |  Q|  W|    E|  R|  T|  Y|  U|    I|  O|  P|  [|  ]|Backs|Ins |
+   * |Tab  |  Q|  W|    E|  R|  T|  Y|  U|    I|  O|  P|  [|  ]|     |Ins |
    * |     |   |   | LGUI|   |   |   |   | RGUI|   |   |   |   |     |    |
    * |--------------------------------------------------------------------|
    * |      |  A|  S|    D|  F|  G|  H|  J|    K|  L|  ;|  '|Enter   |CAPS|
    * |      |_ML|ALT|  CTL|SFT|   |   |SFT| CTL |ALT|   |   |        |    |
    * |--------------------------------------------------------------------|
-   * |    |   |  Z|  X|    C|  V|  B|  N|  M|    ,|  .|  /|      |Up |    |
+   * |BKSP|   |  Z|  X|    C|  V|  B|  N|  M|    ,|  .|  /| Del  |Up |    |
    * |--------------------------------------------------------------------|
    * |    |    |    |       Space/_FL    |    |      |    |  |Lef|Dow|Rig |
    * `--------------------------------------------------------------------'
@@ -42,11 +42,11 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
     [_BL] = KEYMAP( \
-          KC_ESC, KC_1,         KC_2,                KC_3,                 KC_4,                KC_5,   KC_6,   KC_7,               KC_8             ,    KC_9,                KC_0,    KC_MINS, KC_EQL,  KC_BSLS,KC_GRV, KC_NO, \
-          KC_TAB, KC_Q,         KC_W,                MT(MOD_LGUI,KC_E),    KC_R,                KC_T,   KC_Y,   KC_U,               MT(MOD_RGUI,KC_I),    KC_O,                KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,        KC_INS, \
-          KC_NO,  LT(_ML,KC_A), MT(MOD_LALT,KC_S),   MT(MOD_LCTL, KC_D),   MT(MOD_LSFT, KC_F),  KC_G,   KC_H,   MT(MOD_RSFT, KC_J), MT(MOD_RCTL,KC_K),    MT(MOD_LALT,KC_L),   KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,         KC_CAPS,\
-          KC_NO  ,KC_NO,        KC_Z,                KC_X,                 KC_C,                KC_V,   KC_B,   KC_N,               KC_M             ,    KC_COMM,             KC_DOT,  KC_SLSH, KC_NO,   KC_UP,          KC_NO,\
-          KC_NO  ,KC_NO,        KC_NO,                                                     LT(_FL,KC_SPC),                          KC_NO,                KC_NO,               KC_NO,            KC_LEFT, KC_DOWN,        KC_RGHT \
+          KC_ESC,  KC_1,         KC_2,                KC_3,                 KC_4,                KC_5,   KC_6,   KC_7,               KC_8             ,    KC_9,                KC_0,    KC_MINS, KC_EQL,  KC_BSLS,KC_GRV, KC_NO, \
+          KC_TAB,  KC_Q,         KC_W,                MT(MOD_LGUI,KC_E),    KC_R,                KC_T,   KC_Y,   KC_U,               MT(MOD_RGUI,KC_I),    KC_O,                KC_P,    KC_LBRC, KC_RBRC, KC_NO,        KC_INS, \
+          KC_NO,   LT(_ML,KC_A), MT(MOD_LALT,KC_S),   MT(MOD_LCTL, KC_D),   MT(MOD_LSFT, KC_F),  KC_G,   KC_H,   MT(MOD_RSFT, KC_J), MT(MOD_RCTL,KC_K),    MT(MOD_LALT,KC_L),   KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,         KC_CAPS,\
+          KC_BSPC, KC_NO,        KC_Z,                KC_X,                 KC_C,                KC_V,   KC_B,   KC_N,               KC_M             ,    KC_COMM,             KC_DOT,  KC_SLSH, KC_DEL,  KC_UP,          KC_NO,\
+          KC_NO,   KC_NO,        KC_NO,                                                     LT(_FL,KC_SPC),                          KC_NO,                KC_NO,               KC_NO,            KC_LEFT, KC_DOWN,        KC_RGHT \
     ),
     /* Layer _ML: Movement Layer
      * ,----------------------------------------------------------------------.
@@ -56,7 +56,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |----------------------------------------------------------------------|
      * |      |   |   |     |   |   |HOM|Left|Down |Rght|End|   |        |    |
      * |----------------------------------------------------------------------|
-     * |    |   |   |   |     |   |   |   |    |     |Del |   |   McL|MsU|McR |
+     * |    |   |   |   |     |   |   |   |    |     |    |   |   McL|MsU|McR |
      * |----------------------------------------------------------------------|
      * |    |    |    |      Return        |     |      |     |  |MsL|MsD|MsR |
      * `----------------------------------------------------------------------'
@@ -67,7 +67,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_NO,   KC_NO, KC_NO,   KC_NO,\
         KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_PGUP,  KC_UP,    KC_PGDN,  KC_NO,    KC_NO, KC_NO,   KC_NO,          KC_NO,  \
         KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_HOME,  KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_END,   KC_NO, KC_NO,   KC_NO,          KC_NO,\
-        KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_DEL,   KC_NO, KC_BTN1, KC_MS_U, KC_BTN2,\
+        KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_BTN1, KC_MS_U, KC_BTN2,\
         KC_NO,    KC_NO,    KC_NO,                     KC_ENT,                          KC_NO,    KC_NO,    KC_NO,           KC_MS_L, KC_MS_D, KC_MS_R  \
     ),
     /* Layer _FL: Function Layer
