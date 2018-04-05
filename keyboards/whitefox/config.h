cbeddb4b99b073ad8d4012f58f26ea55ba047e23
Arialdo Martini <arialdomartini@gmail.com>
2018-04-05 07:09:37 +0200

    Ignore mod tap interrupt and tapping term to 180 on WhiteFox

--- keyboards/whitefox/config.h
+++ keyboards/whitefox/config.h
@@ -79,4 +79,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define NO_ACTION_MACRO
 //#define NO_ACTION_FUNCTION
 
+#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_TERM 180
+
 #endif
