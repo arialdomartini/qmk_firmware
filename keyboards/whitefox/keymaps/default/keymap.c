773c074dea6ef128590ef6bdbcb3115ec7ffc320
Alessandro Canicatti <ales.canicatti@gmail.com>
2018-04-10 20:17:25 +0200

    Left Gui/E and right gui/I

--- keyboards/whitefox/keymaps/default/keymap.c
+++ keyboards/whitefox/keymaps/default/keymap.c
@@ -16,6 +16,9 @@
 #define MY_S MT(MOD_LALT, KC_S)
 #define MY_L MT(MOD_LALT, KC_L)
 
+#define MY_E MT(MOD_LGUI, KC_E)
+#define MY_I MT(MOD_RGUI, KC_I)
+
 #define MY_FN LT(EXTR, KC_APP)
 #define MY_ESC LT(EXTR, KC_ESC)
 #define MY_ENT LT(FLAY, KC_ENT)
@@ -26,21 +29,21 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,------------------------------------------------------------------------------------------.
      * |MyEs|  1  |  2  |  3 |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |  \  | ` | Ins|
      * |------------------------------------------------------------------------------------------|
-     * |Tab  |    Q|    W|    E|    R|    T|    Y|    U|    I|    O|    P|    [|    ]|Backs|   Del|
+     * |Tab  |    Q|    W|E/Gui|    R|    T|    Y|    U|I/Gui|    O|    P|    [|    ]|Backs|   Del|
      * |------------------------------------------------------------------------------------------|
      * |Esc/F |A/CRS|S/ALT|D/CTL|F/SFT|    G|    H|J/SFT|D/CTL|L/ALT|    ;|    '|Enter/Fun |   PgU|
      * |------------------------------------------------------------------------------------------|
      * |Backspace |    Z|    X|    C|    V|    B|    N|    M|    ,|    .|    /| Delete| Up |   PgD|
      * |------------------------------------------------------------------------------------------|
-     * |    | Gui  |     |           Space                |MyFun|Caps | Gui |    | Lef | Dow | Rig|
+     * |    |      |     |           Space                |MyFun|Caps | Gui |    | Lef | Dow | Rig|
      * `------------------------------------------------------------------------------------------'
      */
     [BASE] = KEYMAP(
         MY_ESC , KC_1   , KC_2   , KC_3 , KC_4 , KC_5 , KC_6 , KC_7 , KC_8 , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSLS, KC_GRV , KC_INS,
-        KC_TAB , KC_Q   , KC_W   , KC_E , KC_R , KC_T , KC_Y , KC_U , KC_I , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSPC         , KC_DEL,
+        KC_TAB , KC_Q   , KC_W   , MY_E , KC_R , KC_T , KC_Y , KC_U , MY_I , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSPC         , KC_DEL,
         MY_CAPS, MY_A   , MY_S   , MY_D , MY_F , KC_G , KC_H , MY_J , MY_K , MY_L   , KC_SCLN, KC_QUOT, KC_NUHS, MY_ENT          , KC_PGUP,
         KC_BSPC, _______, KC_Z   , KC_X , KC_C , KC_V , KC_B , KC_N , KC_M , KC_COMM, KC_DOT , KC_SLSH, KC_DEL          , KC_UP  , KC_PGDN,
-        _______, KC_LGUI, _______,                  KC_SPC                          , KC_CAPS, MY_FN  , _______, KC_LEFT, KC_DOWN, KC_RGHT),
+        _______, _______, _______,                  KC_SPC                          , KC_CAPS, MY_FN  , _______, KC_LEFT, KC_DOWN, KC_RGHT),
 
 
 
