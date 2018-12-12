d4a107accb5cb20bf14e90ed15daac7dc68768dc
Arialdo Martini <arialdomartini@gmail.com>
2018-12-12 15:07:18 +0100

    CapsLock moved to top-right

--- keyboards/tada68/keymaps/gould/keymap.c
+++ keyboards/tada68/keymaps/gould/keymap.c
@@ -1,10 +1,5 @@
-//#include "tada68.h"
 #include QMK_KEYBOARD_H
 
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
 #define _BL 0
 #define _ML 1
 #define _FL 2
@@ -15,23 +10,23 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Layer _BL: (Base Layer) Default Layer
    * ,-----------------------------------------------------------------.
-   * |    | 1|  2|  3 |  4|  5|  6|  7|  8 |  9|  0|  -|  =|  ~ `  |    |
+   * |    | 1|  2|  3 |  4|  5|  6|  7|  8 |  9|  0|  -|  =|  ~ `  |CAPS|
    * |------------------------------------------------------------------|
    * |Tab  |  Q|  W|   E|  R|  T|  Y|  U|  I |  O|  P|  [|  ]|  \  |    |
    * |     |   |   |LGUI|   |   |   |   |RGUI|   |   |   |   |     |    |
    * |------------------------------------------------------------------|
-   * |Esc    |  A|  S|   D|  F|  G|  H|  J|   K|  L|  ;|  '| Enter |CAPS|
+   * |Esc    |  A|  S|   D|  F|  G|  H|  J|   K|  L|  ;|  '| Enter |    |
    * |       |_ML|ALT|CTL |SFT|   |   |SFT|CTL |ALT|   |   |       |    |
    * |------------------------------------------------------------------|
-   * | BackSpc|  Z|  X|   C|  V|  B|  N|  M|  , |  .|  /| Del  | Up|    |
+   * | BackSpc|  Z|  X|   C|  V|  B|  N|  M|  , |  .|  /| Del  | Up|Ins |
    * |------------------------------------------------------------------|
    * |    |    |    |         Space/_FL      |App |   |    |Lef|Dow|Rig |
    * `------------------------------------------------------------------'
    */
 [_BL] = LAYOUT_ansi(
-  KC_NO,    KC_1,         KC_2,                KC_3,                 KC_4,                KC_5,   KC_6,   KC_7,               KC_8,                 KC_9,               KC_0,            KC_MINS, KC_EQL,     KC_GRV,   KC_NO,  \
+  KC_NO,    KC_1,         KC_2,                KC_3,                 KC_4,                KC_5,   KC_6,   KC_7,               KC_8,                 KC_9,               KC_0,            KC_MINS, KC_EQL,     KC_GRV,   KC_CAPS,  \
   KC_TAB,   KC_Q,         KC_W,                MT(MOD_LGUI,KC_E),    KC_R,                KC_T,   KC_Y,   KC_U,               MT(MOD_RGUI,KC_I),    KC_O,               KC_P,            KC_LBRC, KC_RBRC,    KC_BSLS,  KC_NO , \
-  KC_ESC,   LT(_ML,KC_A), MT(MOD_LALT,KC_S),   MT(MOD_LCTL, KC_D),   MT(MOD_LSFT, KC_F),  KC_G,   KC_H,   MT(MOD_RSFT, KC_J), MT(MOD_RCTL,KC_K),    MT(MOD_LALT,KC_L),  KC_SCLN,         KC_QUOT,             KC_ENT ,  KC_CAPS, \
+  KC_ESC,   LT(_ML,KC_A), MT(MOD_LALT,KC_S),   MT(MOD_LCTL, KC_D),   MT(MOD_LSFT, KC_F),  KC_G,   KC_H,   MT(MOD_RSFT, KC_J), MT(MOD_RCTL,KC_K),    MT(MOD_LALT,KC_L),  KC_SCLN,         KC_QUOT,             KC_ENT ,  KC_NO, \
   KC_BSPC,  KC_Z,         KC_X,                KC_C,                 KC_V,                KC_B,   KC_N,   KC_M,               KC_COMM,              KC_DOT,             KC_SLSH,         KC_DEL,              KC_UP  ,  KC_NO, \
   KC_NO,    KC_NO,        KC_NO,                         LT(_FL,KC_SPC),                                                      KC_APP,               KC_NO,              KC_NO,           KC_LEFT,             KC_DOWN,  KC_RGHT),
 
@@ -39,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,--------------------------------------------------------------------.
    * |   |   |   |   |   |   |   |   |    |     |   |   |   |        |    |
    * |--------------------------------------------------------------------|
-   * |     |   |   |   |   |   |    | Home|Up |End |Ins|   |   |     |    |
+   * |     |   |   |   |   |   |    | Home|Up |End |   |   |   |     |    |
    * |--------------------------------------------------------------------|
    * |      |   |   |   |   |   |    |<-  |Dn   | ->|    |   |       |    |
    * |--------------------------------------------------------------------|
