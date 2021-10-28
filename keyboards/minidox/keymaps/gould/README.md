c40aa4f51279f4a01b3c90f61fabc5f3dff04609
Arialdo Martini <arialdomartini@gmail.com>
2021-10-28 10:25:01 +0200

    WIP Gould for MiniDox

---
+++ keyboards/minidox/keymaps/gould/README.md
@@ -0,0 +1,16 @@
+# Compile
+
+I was not able to flash using Docker. I made it work installing `qmk` in Arch via `pacman`.
+
+``` sh
+git fetch --all --recurse-submodules
+```
+
+Then build and flash the firmware with:
+
+``` sh
+qmk flash -kb minidox -km gloud
+```
+
+When asked, click the reset button under master keyboard (the left half), keeping the right part connected.
+
---
+++ keyboards/minidox/keymaps/gould/keymap.c
@@ -0,0 +1,153 @@
+#include QMK_KEYBOARD_H
+
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+#define _BASE 0
+#define _CURS 1
+#define _NUM  2
+#define _SYMB 3
+#define _FUNL 5
+
+#define _ XXXXXXX 
+
+// Defines for task manager and such
+#define CALTDEL LCTL(LALT(KC_DEL))
+#define TSKMGR LCTL(LSFT(KC_ESC))
+
+
+#define MY_F MT(MOD_LSFT, KC_F)
+#define MY_J MT(MOD_RSFT, KC_J)
+#define MY_D MT(MOD_LCTL, KC_D)
+#define MY_K MT(MOD_RCTL, KC_K)
+#define MY_S MT(MOD_LALT, KC_S)
+#define MY_L MT(MOD_LALT, KC_L)
+#define MY_E MT(MOD_LGUI, KC_E)
+#define MY_I MT(MOD_RGUI, KC_I)
+#define MY_DEL LT(_FUNL, KC_DEL)
+#define MY_SPC LT(_SYMB, KC_SPC)
+#define MY_A LT(_CURS, KC_A)
+
+/* #define MY_Q LT(MEDI, KC_Q) */
+/* #define MY_G MT(MOD_HYPR, KC_G) */
+/* #define MY_H MT(MOD_HYPR, KC_H) */
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* Base
+ *
+ * ,----------------------------------.           ,----------------------------------.
+ * |   Q  |   W  |   E  |   R  |   T  |           |   Y  |   U  |   I  |   O  |   P  |
+ * |      |      |Super |      |      |           |      |      | Super|      |      |
+ * |------+------+------+------+------|           |------+------+------+------+------|
+ * |   A  |   S  |   D  |   F  |   G  |           |   H  |   J  |   K  |   L  |   ;  |
+ * | CURS |  Alt | Ctrl |Shift |      |           |      | Shift| Ctrl | Alt  |      |
+ * |------+------+------+------+------|           |------+------+------+------+------|
+ * |   Z  |   X  |   C  |   V  |   B  |           |   N  |   M  |   ,  |   .  |   /  |
+ * `----------------------------------'           `----------------------------------'
+ *                  ,--------------------.    ,------,-------------.
+ *                  |      | BckSp|      |    |      | Del  |      |
+ *                  |      | NUM  |      |    |Space | FUNL |      |
+ *                  `-------------| SYMB |    |SYMB  |------+------.
+ *                                |      |    |      |
+ *                                `------'    `------'
+ */
+[_BASE] = LAYOUT_split_3x5_3( \
+  KC_Q,    KC_W,    MY_E,    KC_R,    KC_T,         KC_Y,    KC_U,    MY_I,    KC_O,    KC_P,    \
+  MY_A,    MY_S,    MY_D,    MY_F,    KC_G,         KC_H,    MY_J,    MY_K,    MY_L,    KC_SCLN, \
+  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
+       _______, LT(_NUM, KC_BSPC),    MO(_SYMB),    MY_SPC,  MY_DEL,  _______                    \
+),
+
+/* CURS
+ *
+ * ,----------------------------------.           ,----------------------------------.
+ * |      |      | Super|      |      |           |      | Home |  Up  | End  |      |
+ * |------+------+------+------+------|           |------+------+------+------+------|
+ * |      |      |      |      |      |           |      |  <-  |      |  ->  |  '   |
+ * |      |  Alt | Ctrl |Shift |      |           |      |      | Down |      |  "   |
+ * |------+------+------+------+------|           |------+------+------+------+------|
+ * |      |      |      |      |      |           |      | PgUp |      |PgDown|      |
+ * `----------------------------------'           `----------------------------------'
+ *                  ,--------------------.    ,------,-------------.
+ *                  |      |      |      |    |      |  Tab |      |
+ *                  `-------------|      |    |Return|------+------.
+ *                                |      |    |      |
+ *                                `------'    `------'
+ */
+[_CURS] = LAYOUT_split_3x5_3( \
+  _,       _,       _______, _,       _,            _,        KC_HOME, KC_UP,   KC_END,   KC_INS,  \
+  _,       _______, _______, _______, _,            _,        KC_LEFT, KC_DOWN, KC_RIGHT, KC_QUOTE,  \
+  _,       _,       _,       _,       _,            _,        KC_PGUP, KC_CAPS,     KC_PGDN,   KC_UNDS,  \
+                    _,       _,       _,            KC_ENTER, KC_TAB, _                 \
+),
+/* NUM
+ *
+ * ,----------------------------------.           ,----------------------------------.
+ * |      |      |      |      |      |           |      |      |      |      |      |
+ * |------+------+------+------+------|           |------+------+------+------+------|
+ * |      |      |      |      |      |           |      |      |      |      |      |
+ * |      |      |      |      |      |           |      |      |      |      |      |
+ * |------+------+------+------+------|           |------+------+------+------+------|
+ * |      |      |      |      |      |           |      |      |      |      |      |
+ * `----------------------------------'           `----------------------------------'
+ *                  ,--------------------.    ,------,-------------.
+ *                  |      |      |      |    |      |  Tab |      |
+ *                  `-------------|      |    |Return|------+------.
+ *                                |      |    |      |
+ *                             tttttttttttttttttttttttttttttttttttttttttttttttttttttttttt  `------'    `------'
+ */
+[_NUM] = LAYOUT_split_3x5_3( \
+  _,       _,       _,       _,       _,          _,       KC_1,    KC_2,      KC_3,    _,   \
+  _, _______, _______, _______, _______,          _,       KC_4,    KC_5,      KC_6,    KC_0,   \
+  _,       _,       _,       _,       _,          _,       KC_7,    KC_8,      KC_9,    _,    \
+                    _,       _______, _,          _,       _,       _ \
+),
+/* FUNL
+ *
+ * ,----------------------------------.           ,----------------------------------.
+ * |      |      |      |      |      |           |      |      |      |      |      |
+ * |------+------+------+------+------|           |------+------+------+------+------|
+ * |      |      |      |      |      |           |      |      |      |      |      |
+ * |      |      |      |      |      |           |      |      |      |      |      |
+ * |------+------+------+------+------|           |------+------+------+------+------|
+ * |      |      |      |      |      |           |      |      |      |      |      |
+ * `----------------------------------'           `----------------------------------'
+ *                  ,--------------------.    ,------,-------------.
+ *                  |      |      |      |    |      |  Tab |      |
+ *                  `-------------|      |    |Return|------+------.
+ *                                |      |    |      |
+ *                                `------'    `------'
+ */
+[_FUNL] = LAYOUT_split_3x5_3( \
+  _,       _,       _,       _,       _,          _,       KC_F1,    KC_F2,      KC_F3,    KC_F11,   \
+  _, _______, _______, _______, _______,          _,       KC_F4,    KC_F5,      KC_F6,    KC_F10,   \
+  _,       _,       _,       _,       _,          _,       KC_F7,    KC_F8,      KC_F9,    KC_F12,    \
+                    _,       KC_ESC,  _,          _,       _,       _ \
+),
+/* SYMB
+ *
+ * ,----------------------------------.           ,----------------------------------.
+ * |      |      |      |      |      |           |      |      |      |      |      |
+ * |------+------+------+------+------|           |------+------+------+------+------|
+ * |      |      |      |      |      |           |      |      |      |      |      |
+ * |      |      |      |      |      |           |      |      |      |      |      |
+ * |------+------+------+------+------|           |------+------+------+------+------|
+ * |      |      |      |      |      |           |      |      |      |      |      |
+ * `----------------------------------'           `----------------------------------'
+ *                  ,--------------------.    ,------,-------------.
+ *                  |      |      |      |    |      |  Tab |      |
+ *                  `-------------|      |    |Return|------+------.
+ *                                |      |    |      |
+ *                                `------'    `------'
+ */
+[_SYMB] = LAYOUT_split_3x5_3( \
+  KC_TILD, _,       _,       _, _,          _, KC_MINS,   KC_PEQL, KC_PPLS, KC_PIPE,   \
+  KC_GRV,  KC_LCBR, KC_LBRC, KC_LPRN, _,          _, KC_RPRN,   KC_RBRC, KC_RCBR, KC_BSLS,   \
+  _,       _,       _,       _,       _,          _,       _,    _,      _,       _,         \
+                    _,       _, _______,          _,       _,       _ \
+)
+};
