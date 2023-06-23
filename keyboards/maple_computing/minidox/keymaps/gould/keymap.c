17637f39d9fdc2476333df7e9d7a4c21c0ea48e5
Arialdo Martini <arialdomartini@gmail.com>
2023-06-23 20:29:38 +0200

    Left thumb = Enter

--- keyboards/maple_computing/minidox/keymaps/gould/keymap.c
+++ keyboards/maple_computing/minidox/keymaps/gould/keymap.c
@@ -48,6 +48,7 @@
 #define MY_I MT(MOD_RGUI, KC_I)
 #define MY_DEL LT(_FUNL, KC_DEL)
 #define MY_SPC LT(_NUM, KC_SPC)
+#define MY_RET LT(_NUM, KC_ENTER)
 #define MY_A LT(_CURS, KC_A)
 #define MY_SCLN LT(_CO, KC_SCLN)
 
@@ -98,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                  ,--------------------.    ,------,-------------.
  *                  |      | BckSp|      |    |      | Del  |      |
  *                  |      | SYMB |      |    |Space | FUNL |      |
- *                  `-------------| NUMB |    |NUMB  |------+------.
+ *                  `-------------| RET  |    |NUMB  |------+------.
  *                                |      |    |      |
  *                                `------'    `------'
  */
@@ -106,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_Q,    KC_W,    MY_E,    KC_R,    KC_T,         KC_Y,    KC_U,    MY_I,    KC_O,    KC_P,    \
   MY_A,    MY_S,    MY_D,    MY_F,    KC_G,         KC_H,    MY_J,    MY_K,    MY_L,    MY_SCLN, \
   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
-    _______,    LT(_SYMB, KC_BSPC),   MY_SPC,       MY_SPC,  MY_DEL,  _______                   \
+    _______,    LT(_SYMB, KC_BSPC),   MY_RET,       MY_SPC,  MY_DEL,  _______                   \
 ),
 
 /* CURS9
