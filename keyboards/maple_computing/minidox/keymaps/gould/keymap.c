5e23dea547391063fcc41a26532f82d4bae657e7
Arialdo Martini <arialdomartini@gmail.com>
2023-10-23 16:49:48 +0200

    Right Super on ; (pinky)

--- keyboards/maple_computing/minidox/keymaps/gould/keymap.c
+++ keyboards/maple_computing/minidox/keymaps/gould/keymap.c
@@ -38,12 +38,12 @@
 #define MY_2 MT(MOD_LALT, KC_2)
 #define MY_F2 MT(MOD_LALT, KC_F2)
 
+#define MY_SCLN MT(MOD_LGUI, KC_SCLN)
 #define MY_L MT(MOD_LALT, KC_L)
 #define MY_9 MT(MOD_LALT, KC_9)
 #define MY_F9 MT(MOD_LALT, KC_F9)
 
 
-#define MY_I MT(MOD_RGUI, KC_I)
 #define MY_DEL LT(_FUNL, KC_DEL)
 #define MY_SPC LT(_NUM, KC_SPC)
 #define MY_RET LT(_CURS, KC_ENTER)
@@ -86,10 +86,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *
  * ,----------------------------------.           ,----------------------------------.
  * |   Q  |   W  |   E  |   R  |   T  |           |   Y  |   U  |   I  |   O  |   P  |
- * |      |      |      |      |      |           |      |      | Super|      |      |
+ * |      |      |      |      |      |           |      |      |      |      |      |
  * |------+------+------+------+------|           |------+------+------+------+------|
  * |   A  |   S  |   D  |   F  |   G  |           |   H  |   J  |   K  |   L  |   ;  |
- * | Super|  Alt | Ctrl |Shift |      |           |      | Shift| Ctrl | Alt  |      |
+ * | Super|  Alt | Ctrl |Shift |      |           |      | Shift| Ctrl | Alt  | Super|
  * |------+------+------+------+------|           |------+------+------+------+------|
  * |   Z  |   X  |   C  |   V  |   B  |           |   N  |   M  |   ,  |   .  |   /  |
  * `----------------------------------'           `----------------------------------'
@@ -101,8 +101,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                `------'    `------'
  */
 [_BASE] = LAYOUT_split_3x5_3( \
-  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    MY_I,    KC_O,    KC_P,    \
-  MY_A,    MY_S,    MY_D,    MY_F,    KC_G,         KC_H,    MY_J,    MY_K,    MY_L,    KC_SCLN, \
+  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
+  MY_A,    MY_S,    MY_D,    MY_F,    KC_G,         KC_H,    MY_J,    MY_K,    MY_L,    MY_SCLN, \
   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
     _______,    LT(_SYMB, KC_BSPC),   MY_RET,       MY_SPC,  MY_DEL,  _______                   \
 ),
