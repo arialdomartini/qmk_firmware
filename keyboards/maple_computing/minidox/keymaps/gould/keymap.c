b71c182db156b1a034621f31d446446f69a091f7
Arialdo Martini <arialdo.martini@gmail.com>
2024-07-01 07:32:24 +0200

    >>= on ring finger

--- keyboards/maple_computing/minidox/keymaps/gould/keymap.c
+++ keyboards/maple_computing/minidox/keymaps/gould/keymap.c
@@ -64,7 +64,8 @@
 
 enum custom_keycodes {
     MYARROW = SAFE_RANGE,
-    MYDARROW
+    MYDARROW,
+    MYBIND
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -77,6 +78,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case MYDARROW:
         if (record->event.pressed) {
           SEND_STRING("=>");
+        }
+        break;
+    case MYBIND:
+        if (record->event.pressed) {
+          SEND_STRING(">>=");
         }
         break;
   };
@@ -186,7 +192,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------|           |------+------+------+------+------|
  * |  <   |  {   |  [   |   (  |      |           |      |  )   |   ]  |  }   |   >  |
  * |------+------+------+------+------|           |------+------+------+------+------|
- * |      |      |      |      |      |           |      |  ->  |  =>  |      |  \   |
+ * |      |      |      |      |      |           |      |  ->  |  =>  |  >>= |  \   |
  * `----------------------------------'           `----------------------------------'
  *                  ,--------------------.    ,------,-------------.
  *                  |      |      |      |    |      |      |      |
@@ -197,7 +203,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_SYMB] = LAYOUT_split_3x5_3( \
   _,       _,        _,        _,        _,          _,   KC_UNDS,   KC_ASTR,   KC_TILD,  KC_PIPE,   \
   KC_LT,   KC_LCBR,  KC_LBRC,  KC_LPRN,  _,          _,   KC_RPRN,   KC_RBRC,   KC_RCBR,  KC_GT,     \
-  _,       _,        _,        _,        _,          _,   MYARROW,   MYDARROW,  _,        KC_BSLS,   \
+  _,       _,        _,        _,        _,          _,   MYARROW,   MYDARROW,  MYBIND,   KC_BSLS,   \
                      _,        _,       _______,          _,         _,         _ \
 ),
 /* FLASH
